f0e0496a0b9f620bd4baeb7baf189c09d0fb494b
Rayasxw <maksatbekovarayana@gmail.com>
2025-05-09 18:59:36 +0600

    header

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -2,18 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './MainPage.module.scss'
 import Instruction from '../instruction/Inctruction'
+import Header from '../../components/Header'
 const MainPage = () => {
   return (
     <div className={styles.bgc}>
+        <Header/>
         <div className={styles.container}>
-        <header>
-            <h1>COINHUNTERS</h1>
-            <ul>
-                <Link to='/liderBoard'>
-                    <li>leaderBoard</li>
-                </Link>
-            </ul>
-        </header>
         <main>
             <h2>COINHUNTERS</h2>
             <p>Исследуй карту и открывай новые способности</p>
